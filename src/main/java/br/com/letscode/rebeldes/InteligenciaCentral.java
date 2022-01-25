f2586ad18e7acdec833230b1db10bd1de2d3f86c
Tulio Colmanetti <tulio.colmanetti@gertec.com.br>
2022-01-25 18:17:54 -0400

    Refactored constructor and implemented method for accepting or refusing rebels

--- src/main/java/br/com/letscode/rebeldes/InteligenciaCentral.java
+++ src/main/java/br/com/letscode/rebeldes/InteligenciaCentral.java
@@ -1,17 +1,31 @@
 package br.com.letscode.rebeldes;
 
-import lombok.Builder;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-@Builder
 public class InteligenciaCentral {
     @Getter
     List<Rebelde> rebeldes;
 
-    public boolean solicitarIngressoDeRebelde(Rebelde rebelde) {
-        return false;
+    public InteligenciaCentral(){
+        this.rebeldes = new ArrayList<>();
     }
 
+    public boolean solicitarIngressoDeRebelde(Rebelde rebelde) {
+        int randNumber = new Random().nextInt(10);
+
+        if(randNumber <= 5) {
+            this.rebeldes.add(rebelde);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void gerarRelatorioDeRebeldes() {
+
+    }
 }
